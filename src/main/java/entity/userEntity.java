0ddfb67d371fdddad2e1c255b9a5638add7d2c23
Diego Vega <115362981+DiegoVUsach@users.noreply.github.com>
2026-04-07 17:21:32 -0400

    test

---
+++ src/main/java/entity/userEntity.java
@@ -0,0 +1,9 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+public class userEntity {
+}
