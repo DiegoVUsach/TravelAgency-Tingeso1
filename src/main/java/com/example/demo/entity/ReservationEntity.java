92484075e25ab6a1ba1fd9247019ed274a854a68
Diego Vega <diego.vega.f@usach.cl>
2026-05-02 03:18:47 -0400

    e4 start

--- src/main/java/com/example/demo/entity/ReservationEntity.java
+++ src/main/java/com/example/demo/entity/ReservationEntity.java
@@ -1,4 +1,42 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@Table(name = "Reservations")
+
 public class ReservationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // TBI keycloak stuff here
+    @Column(nullable = false)
+    private String userEmail;
+
+    // check later 02 05 2026 from hero dowm
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bundle_id", nullable = false)
+    private BundleEntity bundle;
+
+    @Column(nullable = false)
+    private Integer numberOfPassengers;
+
+    @Column(nullable = false)
+    private LocalDate reservationDate;
+
+    @Column(nullable = false)
+    private Integer totalAmount; // Monto final tras aplicar los descuentos
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ReservationState state;
 }
---
+++ src/main/java/com/example/demo/entity/ReservationState.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+
+public enum ReservationState {
+    PENDING_PAYMENT,
+    CONFIRMED,
+    CANCELED
+}
