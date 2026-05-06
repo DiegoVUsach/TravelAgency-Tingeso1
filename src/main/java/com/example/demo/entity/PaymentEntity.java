ca7d156ae6c7a6bcca2b4dd0c1fc0e9ceab9c3b3
Diego Vega <115362981+DiegoVUsach@users.noreply.github.com>
2026-05-06 18:11:42 -0400

    e5 progress

--- src/main/java/com/example/demo/entity/PaymentEntity.java
+++ src/main/java/com/example/demo/entity/PaymentEntity.java
@@ -1,4 +1,35 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Payments")
 public class PaymentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @OneToOne //one payment to one reservation
+    @JoinColumn(name = "reservation_id", nullable = false, unique = true)
+    private ReservationEntity reservation;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @Column(nullable = false)
+    private LocalDateTime paymentDate;
+
+    // simulated payment method
+    @Column(nullable = false)
+    private String paymentMethod;
+
 }
