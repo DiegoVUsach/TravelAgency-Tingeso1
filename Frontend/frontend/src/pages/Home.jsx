15edadee0da75e59590a37b2c73c434f7c22c8c4
Diego Vega <diego.vega.f@usach.cl>
2026-05-10 21:17:03 -0400

    front progress

---
+++ Frontend/frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to TravelAgency</h1>
+      <p>This is the initial setup for Tingeso 2026-1.</p>
+    </div>
+  );
+}
+
+export default Home;
